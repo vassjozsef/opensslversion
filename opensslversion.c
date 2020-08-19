34d16f068bcf7cd098db1455c1ccabef05fe8d77
Jozsef Vass <jozsef@discordapp.com>
2020-08-19 14:23:48 -0700

    add hmac

--- opensslversion.c
+++ opensslversion.c
@@ -1,17 +1,28 @@
-// OSX:  clang opensslversion.c -I /usr/local/opt/openssl/include -L /usr/local/opt/openssl/lib -lssl
+// OSX:  clang opensslversion.c -I /usr/local/opt/openssl/include -L /usr/local/opt/openssl/lib -lssl -lcrypto
 // OSX uses brewed openssl
-#include "openssl/ssl.h"
+// Linux: clang-7 opensslversion.c -lssl
+#include <openssl/ssl.h>
+#include <openssl/hmac.h>
+#include <openssl/sha.h>
 
 int main(void) {
-  
+
   printf("OpenSSL version: 0x%lx\n", OPENSSL_VERSION_NUMBER);
   printf("OpenSSL version: %s\n", OPENSSL_VERSION_TEXT);
 
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
-    SSL_library_init();
+  SSL_library_init();
 #else
-    OPENSSL_init_ssl(0, NULL);
+  OPENSSL_init_ssl(0, NULL);
 #endif
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+  HMAC_CTX stackctx;
+  HMAC_CTX *ctx = &stackctx;
+  HMAC_CTX_init (ctx);
+#else
+  HMAC_CTX *ctx = HMAC_CTX_new ();
+#endif /* OPENSSL_VERSION_NUMBER */
 
   return 0;
 }
